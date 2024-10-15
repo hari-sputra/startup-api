19c4353ade5f6ac0d579fa9c5cd6ab8039f3a444
hari saputra <hari.saputra.dev@gmail.com>
2024-10-15 14:02:42 +0700

    [update] entity and repository campaign

--- API/campaign/entity.go
+++ API/campaign/entity.go
@@ -1,1 +1,28 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	ID               int
+	UserID           int
+	Name             string
+	TitleDescription string
+	Description      string
+	GoalAmount       int
+	CurrentAmount    int
+	Perks            string
+	BackerCount      int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	CampaignImages   []CampaignImage
+}
+
+type CampaignImage struct {
+	ID         int
+	CampaignID int
+	FileName   string
+	IsPrimary  bool
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
--- API/campaign/repository.go
+++ API/campaign/repository.go
@@ -1,1 +1,38 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type CampaignRepository interface {
+	FindAll() ([]Campaign, error)
+	FindByUserID(userID int) ([]Campaign, error)
+}
+
+type campaignRepo struct {
+	db *gorm.DB
+}
+
+func NewCampaignRepository(db *gorm.DB) *campaignRepo {
+	return &campaignRepo{db}
+}
+
+func (r *campaignRepo) FindAll() ([]Campaign, error) {
+	var campaigns []Campaign
+
+	err := r.db.Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
+
+func (r *campaignRepo) FindByUserID(userID int) ([]Campaign, error) {
+	var campaigns []Campaign
+
+	err := r.db.Where("user_id = ?", userID).Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
